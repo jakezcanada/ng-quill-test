cecf65c4ae32e6cd0639f07711e84857fe5f4cfa
Jake <jakezcanada@gmail.com>
2024-01-03 20:43:31 -0500

    update dependencies

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
+import { QuillModule } from 'ngx-quill'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    QuillModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
